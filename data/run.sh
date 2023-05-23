1eee341b1f2ffcb8bfa299053f70e40a6236c7a4
Dieter Best <dieterbest@gmail.com>
2023-05-23 16:39:49 -0700

    add example how to run docker image

--- data/run.sh
+++ data/run.sh
@@ -2,19 +2,27 @@
 # https://www.biostars.org/p/50963/
 # Note: put tools directory in PATH
 
-DATA=$HOME/Analysis/wgs_pipeline/data/
-R1=$DATA/ERR552797_1.fastq.gz
-R2=$DATA/ERR552797_2.fastq.gz
+FASTQ=$HOME/Analysis/wgs_pipeline/data/
+R1=$FASTQ/ERR552797_1.fastq.gz
+R2=$FASTQ/ERR552797_2.fastq.gz
 SAMPL=ERR552797
 REF=$VARPIPE/ref2.fa
 
-#cp ../tools/config.yml . 
+#cp ../tools/config.yml  
 #Varpipeline -q $R1 -q2 $R2 -r $REF -n $SAMPL -o $SAMPL -a -v -t 8 --keepfiles
 
-TOOLS=/NCHHSTP-DTBE-Varpipe-WGS/tools
-REF=$TOOLS/ref2.fa
-DAT=/mnt/data
-R1=$DAT/ERR552797_1.fastq.gz
-R2=$DAT/ERR552797_2.fastq.gz
-docker run --rm -v $DATA:$DAT -w $TOOLS dbest/varpipe:latest Varpipeline -q $R1 -q2 $R2 -r $REF -n $SAMPL -v -a --keepfiles
+#TOOLS=/NCHHSTP-DTBE-Varpipe-WGS/tools
+TOOLS=$HOME/Analysis/varpipe/tools
+TLS=/mnt/tools
+REF=$TLS/ref2.fa
+
+FASTQ=$HOME/Analysis/wgs_pipeline/data/
+FSQ=/mnt/fastq
+R1=$FSQ/ERR552797_1.fastq.gz
+R2=$FSQ/ERR552797_2.fastq.gz
+
+DATA=$HOME/Analysis/varpipe/data
+WRK=/mnt/data
+
+docker run --rm -v $FASTQ:$FSQ -v $TOOLS:$TLS -v $DATA:$WRK -w $WRK dbest/varpipe:latest Varpipeline -q $R1 -q2 $R2 -r $REF -n $SAMPL -v -a --keepfiles
 
